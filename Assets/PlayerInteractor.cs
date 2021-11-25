d64bee1347cf19f7abfc51a832154558192b8fcd
Ilistratov <i.listratov@yandex.ru>
2021-11-25 19:47:11 +0300

    add PlayerInteractor implementation

--- Assets/PlayerInteractor.cs
+++ Assets/PlayerInteractor.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerInteractor : MonoBehaviour
 {
-    public void PerfomSelectedInteraction() { } //TODO
-    public void SelectNextInteraction() { } // TODO
-    public int RegisterInteraction(GenericInteraction interaction) { return 0; } // TODO
-    public void UnregisterInteraction(int interactionToken) { } // TODO
+    private SortedList mAvailableInteractions;
+    private int mNextRegistrationToken;
+    private int mSelectedInteractionIndex;
+
+    public void PerfomSelectedInteraction() {
+        if (mSelectedInteractionIndex < mAvailableInteractions.Count) {
+            mAvailableInteractions.GetByIndex(mSelectedInteractionIndex);
+        }
+    }
+    
+    public void SelectNextInteraction() {
+        mSelectedInteractionIndex += 1;
+        if (mSelectedInteractionIndex == mAvailableInteractions.Count) {
+            mSelectedInteractionIndex = 0;
+        }
+    }
+    
+    public int RegisterInteraction(GenericInteraction interaction) {
+        mAvailableInteractions.Add(mNextRegistrationToken, interaction);
+        mNextRegistrationToken += 1;
+        return mNextRegistrationToken - 1;
+    }
+    
+    public void UnregisterInteraction(int interactionToken) {
+        if (mSelectedInteractionIndex + 1 == mAvailableInteractions.Count) {
+            mSelectedInteractionIndex = 0;
+        }
+        mAvailableInteractions.Remove(interactionToken);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mAvailableInteractions = new SortedList();
+        mNextRegistrationToken = 1;
+        mSelectedInteractionIndex = 0;
     }
 
     // Update is called once per frame
