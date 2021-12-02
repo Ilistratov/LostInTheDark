e226ac1f951b18ab63f404cc101a3f8696dcafeb
Ilistratov <i.listratov@yandex.ru>
2021-12-03 01:54:23 +0300

    cleanup #2

--- Assets/Scripts/DoorInteractor.cs
+++ Assets/Scripts/DoorInteractor.cs
@@ -47,15 +47,4 @@ public class DoorInteractor : GenericInteraction
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
