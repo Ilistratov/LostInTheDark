6329918e71bc96e4017e4bed91be7a5fac9bdeb1
MonsterOnLeash <660867@mail.ru>
2021-12-08 19:39:08 +0300

    light-interactor-3

--- Assets/Scripts/LightInteractor.cs
+++ Assets/Scripts/LightInteractor.cs
@@ -5,20 +5,20 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class LightInteractor : MonoBehaviour
 {
-    public float lighted;
+    public float lightLevel;
     public List<GameObject> collidedLights;
 
     // Start is called before the first frame update
     void Start()
     {
-        lighted = 0;
+        lightLevel = 0;
         collidedLights = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        lighted = 0;
+        lightLevel = 0;
         if (collidedLights.Count != 0)
         {
             collidedLights.ForEach(CheckForLightRay);
@@ -50,13 +50,17 @@ public class LightInteractor : MonoBehaviour
         ContactFilter2D filter = new ContactFilter2D();
         filter.NoFilter();
         int numberOfHits = Physics2D.Raycast(transform.position, direction, filter, allObjectsBetween, distance);
-        lighted += 1/(distance * distance);
+        float localDistance = 1 / (distance * distance);
+        lightLevel += 1/(distance * distance);
         for (int i = 0; i < numberOfHits; i++)
         {
             if (allObjectsBetween[i].collider.gameObject.GetComponent<ShadowCaster2D>() != null)
             {
-                lighted = 0;
+                return;
             }
         }
+        //lightLevel += 1 / (distance * distance);
+        float rad = source.GetComponent<CircleCollider2D>().radius;
+        lightLevel += 1 - (distance * distance) / (rad * rad);
     }
 }
